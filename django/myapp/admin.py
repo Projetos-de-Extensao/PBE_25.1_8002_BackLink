8ee2f49c2e9ffe86388ff9579fe9ce2caa64beac
GuilhermeLopes06 <guilhermedemoraes54@gmail.com>
2025-05-28 15:32:52 -0300

    MoradorAdmin acrescentado no Admin.py

--- django/myapp/admin.py
+++ django/myapp/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from myapp.models import Domicilio, Morador
 
-""" @admin.register(Morador)
 class MoradorAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'sobrenome', 'sexo', 'idade', 'domicilios_list')
-
-    def domicilios_list(self, obj):
-        return ", ".join(str(dom) for dom in obj.domicilios.all())
-    domicilios_list.short_description = "Domicilios" """
+    list_display = ('nome', 'sobrenome', 'sexo', 'idade', 'relacao_com_responsavel')
+    list_filter = ('sexo', 'idade', 'relacao_com_responsavel')
+    search_fields = ('nome', 'sobrenome')
 
 admin.site.register(Domicilio)
-
-admin.site.register(Morador)
+admin.site.register(Morador, MoradorAdmin)
